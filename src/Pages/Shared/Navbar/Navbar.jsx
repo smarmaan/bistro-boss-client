3fb95c7643c68759aa12fedb3279d2b7e8698da9
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-27 02:33:05 +0600

    navbar template implemented

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,55 @@
-
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const navOptions = (
+    <>
+    <div className="md:flex gap-10">
+
+
+    <li>HOME</li>
+      <li>CONTACT US</li>
+      <li>DASHBOARD</li>
+      <li>OUR MENU</li>
+      <li>OUR SHOP</li>
+    </div>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navOptions}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">SIGN OUT</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
