430868659a952efcfb6ed9a3e5b85d4adf1762ff
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-27 02:46:07 +0600

    Carousel added in banner

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-10 max-w-screen-2xl bg-opacity-30 bg-black text-white">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
