5dd0cf13d7e1751407e55fe02c45d8285a47553c
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-27 02:03:25 +0600

    bistro-boss-client footer section complete

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-screen-2xl mx-auto">
+    <RouterProvider router={router} />
+  </div>
+);
