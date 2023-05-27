fc973de40b26d16ea8880e5e5a01f22396b90592
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-27 20:39:57 +0600

    Button added on our menu check it out section

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -17,13 +17,19 @@ const PopularMenu = () => {
   console.log(menu);
 
   return (
-    <section>
+    <section className="mb-28">
       <SectionTitle heading="FROM OUR MENU" subheading="---Check it out---" />
 
       <div className="grid md:grid-cols-2 gap-10 my-12">
         {menu.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
+      </div>
+
+      <div className="flex justify-center">
+        <button className="btn border-[#1F2937] border-b-4 border-0 text-xl text-[#1F2937] font-medium text-center uppercase">
+          View Full Menu
+        </button>
       </div>
     </section>
   );
