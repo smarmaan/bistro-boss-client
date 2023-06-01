8caf6c683e9a77056b2009784f72ccc7b0049ac8
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-01 12:35:07 +0600

    Price added dynamically in each card

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -6,10 +6,13 @@ const FoodCard = ({ item }) => {
       <figure>
         <img src={image} className="w-full" alt="Salad" />
       </figure>
+      <p className="absolute right-0 mr-4 mt-4 px-5 py-2 rounded-xl bg-[#111827] text-white">
+        ${price}
+      </p>
+
       <div className="card-body">
         <h2 className="card-title justify-center mt-8">{name}</h2>
         <p>{recipe}</p>
-        <p>{price}</p>
 
         <div className="card-actions justify-center my-6">
           <button className="btn bg-[#E8E8E8] hover:bg-[#1F2937] border-[#BB8506] border-b-4 hover:border-b-0 border-0 text-[#BB8506] ">
