56d95376ea7fc6a877f01bbf8d21fccd0311bdac
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-04 10:53:20 +0600

    delete cart function implemented

--- src/Pages/Dashboard/MyCart.jsx
+++ src/Pages/Dashboard/MyCart.jsx
@@ -1,16 +1,45 @@
 import { Helmet } from "react-helmet-async";
 import UseCart from "../../Hooks/UseCart";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import { BsTrash3Fill } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
-  const [cart] = UseCart();
+  const [cart, refetch] = UseCart();
   console.log(cart);
   const total = cart.reduce((acc, item) => acc + item.price, 0);
 
   const totalFixed = total.toFixed(2);
 
+  const handleDeleteProduct = (item) => {
+    console.log(item);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: "delete",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
-    <section>
+    <section className="w-full">
       <Helmet>
         <title>Bistro Boss | My Cart </title>
       </Helmet>
@@ -22,7 +51,7 @@ const MyCart = () => {
         />
       </div>
       <div>
-        <div className="uppercase flex justify-evenly items-center font-bold my-10 md:text-2xl">
+        <div className="uppercase flex justify-evenly items-center font-bold my-10 md:text-xl">
           <h1>Total orders:{cart.length} </h1>
           <h1>Total price:$ {totalFixed}</h1>
           <button className="btn btn-xs bg-[#D1A054] text-white border-0 hover:bg-[#e46905]">
@@ -34,60 +63,47 @@ const MyCart = () => {
 
  */}
         <>
-          <div className="overflow-x-auto">
-            <table className="table">
+          <div className="overflow-x-auto w-full flex justify-center">
+            <table className="table ">
               {/* head */}
               <thead>
                 <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <th>Name</th>
-                  <th>Job</th>
-                  <th>Favorite Color</th>
-                  <th></th>
+                  <th>No.</th>
+                  <th>ITEM IMAGE</th>
+                  <th>ITEM NAME</th>
+                  <th>PRICE</th>
+                  <th>ACTION</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="">
                 {/* row 1 */}
-                <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <td>
-                    <div className="flex items-center space-x-3">
+
+                {cart.map((item, index) => (
+                  <tr key={item._id}>
+                    <th>{index + 1}</th>
+                    <td>
                       <div className="avatar">
                         <div className="mask mask-squircle w-12 h-12">
                           <img
-                            src="/tailwind-css-component-profile-2@56w.png"
+                            src={item.image}
                             alt="Avatar Tailwind CSS Component"
                           />
                         </div>
                       </div>
-                      <div>
-                        <div className="font-bold">Hart Hagerty</div>
-                        <div className="text-sm opacity-50">United States</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>
-                    Zemlak, Daniel and Leannon
-                    <br />
-                    <span className="badge badge-ghost badge-sm">
-                      Desktop Support Technician
-                    </span>
-                  </td>
-                  <td>Purple</td>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
-                  </th>
-                </tr>
+                    </td>
+                    <td>{item.name}</td>
+                    <td> {item.price}</td>
+                    <td>
+                      <button
+                        onClick={() => handleDeleteProduct(item)}
+                        className="btn btn-ghost btn-xs bg-[#B91C1C]"
+                      >
+                        <BsTrash3Fill className=" text-white" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
-              {/* foot */}
             </table>
           </div>
         </>
