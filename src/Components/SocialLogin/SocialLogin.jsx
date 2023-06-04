80017f0d51a3fc6813f5b2e56d3e148f731546f8
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-04 12:41:22 +0600

    redirect - google login and user store in DB once

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -30,13 +30,11 @@ const SocialLogin = () => {
         body: JSON.stringify(saveUser),
       })
         .then((res) => res.json())
-        .then((data) => {
-          if (data.insertedId) {
-            navigate(from, { replace: true });
-          }
+        .then(() => {
+          navigate(from, { replace: true });
         });
 
-      navigate(from, { replace: true });
+      //   navigate(from, { replace: true });
     });
   };
 
